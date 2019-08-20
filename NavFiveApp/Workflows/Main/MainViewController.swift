a70d64b560b4c675c4e5655a46de9737fb1828e6
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-08-20 20:41:37 +0300

    I... just don't get it...

--- NavFiveApp/Workflows/Main/MainViewController.swift
+++ NavFiveApp/Workflows/Main/MainViewController.swift
@@ -4,4 +4,20 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         view.backgroundColor = .yellow
     }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.view = UIView()
+        print("INIT coder")
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        self.view = UIView()
+        print("INIT reg")
+    }
+    
+    deinit {
+        print("DEINIT")
+    }
 }
