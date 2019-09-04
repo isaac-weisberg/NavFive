e8054a6e52586e76be9adbf04fcd18a55642c1e9
Isaac Weisberg <isaac_weisberg@outlook.com>
2019-09-04 13:06:51 +0300

    Implement the first model of the silly app

---
+++ Silly/DetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DetailViewController.swift
+//  Silly
+//
+//  Created by MacBook on 9/4/19.
+//  Copyright © 2019 Isaac Weisberg. All rights reserved.
+//
+
+import Foundation
---
+++ Silly/TopViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TopViewController.swift
+//  Silly
+//
+//  Created by MacBook on 9/4/19.
+//  Copyright © 2019 Isaac Weisberg. All rights reserved.
+//
+
+import Foundation
